17de2e6df6e4ede31266d8582635139f0dfa991e
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2024-08-15 07:02:02 -0700

    Additional Footer Content

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,68 +1,111 @@
 <template>
-  <img src="/assets/images/footersky.svg" class="w-100">
-  <footer class="relative bg-[#FFBA00]  py-8">
+  <div>
+    <img src="/assets/images/footersky.svg" class="w-100">
+    <footer class="relative bg-[#FFBA00]  py-8">
 
 
-    <!-- Footer Content -->
-    <div style=""
-      class="relative z-10 max-w-7xl mx-auto px-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-      <!-- Section 1 -->
-      <div>
-        <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For You</h3>
-        <ul>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Open an Account</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a loan</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Invest</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a card</li>
-        </ul>
+      <!-- Footer Content -->
+      <div style=""
+        class="relative z-10 max-w-7xl mx-auto px-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+        <!-- Section 1 -->
+        <div>
+          <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For You</h3>
+          <ul>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Open an Account</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a loan</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Invest</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a card</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For Your Biashara</h3>
+          <ul>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Open an Account</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a loan</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Trade Finance</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For Corprate</h3>
+          <ul>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Accounts</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Financing</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Services</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">Quick Links</h3>
+          <ul>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">FAQs</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Careers</li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Complaint Handling
+            </li>
+            <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Service Charter</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">Banking Hours</h3>
+          <ul class="font-light">
+            <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
+              Mon – Fri: 8.30 am - 4.00 pm
+            </li>
+            <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
+              Sat: 8.30 am - 12.00 pm
+            </li>
+            <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
+              Sun: Closed
+            </li>
+            <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
+              Public Holidays: Closed
+            </li>
+          </ul>
+        </div>
+
       </div>
+    </footer>
+    <div class="bg-[#3c251f] text-white py-4">
+      <div class="container max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
+        <!-- Left Side: Links -->
+        <div class="flex flex-wrap justify-center md:justify-start space-x-4 text-xs mb-4 md:mb-0">
+          <a href="#" class="hover:underline">Terms and Conditions</a>
+          <span>|</span>
+          <a href="#" class="hover:underline">Disclaimer</a>
+          <span>|</span>
+          <a href="#" class="hover:underline">Tariff Guide</a>
+          <span>|</span>
+          <a href="#" class="hover:underline">Internet Privacy</a>
+        </div>
 
-      <div>
-        <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For Your Biashara</h3>
-        <ul>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Open an Account</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Get a loan</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Trade Finance</li>
-        </ul>
+        <!-- Right Side: Links and Social Media Icons -->
+        <div class="flex flex-col md:flex-row items-center space-y-4 md:space-y-0">
+          <div class="flex flex-wrap justify-center md:justify-start space-x-4 text-xs mb-4 md:mb-0">
+            <a href="#" class="hover:underline">Terms and Conditions</a>
+            <span>|</span>
+            <a href="#" class="hover:underline">Disclaimer</a>
+            <span>|</span>
+
+            <a href="#" class="hover:underline">Internet Privacy Policy</a>
+          </div>
+
+          <!-- Social Media Icons -->
+          <div class="flex space-x-4 ml-6">
+            <a href="#" class="hover:opacity-75">
+              <img src="/assets/images/facebook.png" alt="Facebook" class="w-6 h-6">
+            </a>
+            <a href="#" class="hover:opacity-75">
+              <img src="/assets/images/twitter.png" alt="Twitter" class="w-6 h-6">
+            </a>
+            <a href="#" class="hover:opacity-75">
+              <img src="/assets/images/instagram.png" alt="Instagram" class="w-6 h-6">
+            </a>
+            <!-- Add more social media icons as needed -->
+          </div>
+        </div>
       </div>
-
-      <div>
-        <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">For Corprate</h3>
-        <ul>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Accounts</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Financing</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Services</li>
-        </ul>
-      </div>
-
-      <div>
-        <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">Quick Links</h3>
-        <ul>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">FAQs</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Careers</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Complaint Handling</li>
-          <li class=" text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">Service Charter</li>
-        </ul>
-      </div>
-
-      <div>
-        <h3 class="text-lg font-black text-[#3C251F] font-bold mb-2">Banking Hours</h3>
-        <ul class="font-light">
-          <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
-            Mon – Fri: 8.30 am - 4.00 pm
-          </li>
-          <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
-            Sat: 8.30 am - 12.00 pm
-          </li>
-          <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
-            Sun: Closed
-          </li>
-          <li class="text-[#3C251F] font-light hover:translate-x-4 hover:transition-transform">
-            Public Holidays: Closed
-          </li>
-        </ul>
-      </div>
-
     </div>
-  </footer>
+  </div>
 </template>
